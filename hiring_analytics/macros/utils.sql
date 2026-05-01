23c4c49bb990d232a7a4f4742b0edc6f75eb0ca3
Kseniia Pestova <kseniia_pestova@epam.com>
2026-05-01 20:08:26 +0400

    schema changed to dev

--- hiring_analytics/macros/utils.sql
+++ hiring_analytics/macros/utils.sql
@@ -9,9 +9,3 @@
     from {{ source_cte }}
 
 {% endmacro %}
-
-{% macro generate_schema_name(custom_schema_name, node) -%}
-
-    {{ custom_schema_name if custom_schema_name else target.schema }}
-    
-{%- endmacro %}
