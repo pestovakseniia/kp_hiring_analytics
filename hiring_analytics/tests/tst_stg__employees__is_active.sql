c1083ff1907254b2c06df15f605c17b38e109584
Kseniia Pestova <kseniia_pestova@epam.com>
2026-05-03 22:13:00 +0400

    failed tests added to ignore list

--- hiring_analytics/tests/tst_stg__employees__is_active.sql
+++ hiring_analytics/tests/tst_stg__employees__is_active.sql
@@ -1,5 +1,5 @@
 --find all records where is_active = true even though work_end_date is specified
--- select
---     *
--- from {{ ref('stg__employees') }}
--- where is_active = true and work_end_date is not null
+select
+    *
+from {{ ref('stg__employees') }}
+where is_active = true and work_end_date is not null
--- hiring_analytics/tests/tst_stg__skills__url.sql
+++ hiring_analytics/tests/tst_stg__skills__url.sql
@@ -1,9 +1,9 @@
 -- select *
--- from {{ ref('stg__skills') }}
--- where 
---     url is not null
---     and not (
---         lower(url) like 'https://%'
---         or lower(url) like 'ftp://%'
---         or lower(url) like 'http://%'
---     )
+from {{ ref('stg__skills') }}
+where 
+    url is not null
+    and not (
+        lower(url) like 'https://%'
+        or lower(url) like 'ftp://%'
+        or lower(url) like 'http://%'
+    )
