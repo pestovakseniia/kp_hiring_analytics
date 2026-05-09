e034620368247f6a3e9b7825913a98953de5975b
Kseniia Pestova <kseniia_pestova@epam.com>
2026-05-10 02:50:23 +0400

    stg__date to be populated continuously

--- hiring_analytics/models/staging/stg__date.sql
+++ hiring_analytics/models/staging/stg__date.sql
@@ -1,54 +1,31 @@
 {{ config(alias='stg_date', materialized='view') }}
 
 with source_candidates as (
-
-    select
-        _created_micros,
-        _updated_micros
+    select _created_micros, _updated_micros
     from {{ source('snowflake_sources', 'candidates') }}
-
 ),
 
 source_employees as (
-
-    select
-        work_start_micros,
-        work_end_micros,
-        _created_micros,
-        _updated_micros
+    select work_start_micros, work_end_micros, _created_micros, _updated_micros
     from {{ source('snowflake_sources', 'employees') }}
-
 ),
 
 source_job_functions as (
-
-    select
-        _created_micros,
-        _updated_micros
+    select _created_micros, _updated_micros
     from {{ source('snowflake_sources', 'job_functions') }}
-
 ),
 
 source_skills as (
-
-    select
-        _created_micros,
-        _updated_micros
+    select _created_micros, _updated_micros
     from {{ source('snowflake_sources', 'skills') }}
-
 ),
 
 source_interviews as (
-
-    select
-        _created_micros,
-        _updated_micros
+    select _created_micros, _updated_micros
     from {{ source('snowflake_sources', 'interviews') }}
-
 ),
 
 sources_united as (
-
     select to_date(_created_micros) as date from source_candidates
     union
     select to_date(_updated_micros) from source_candidates
@@ -72,62 +49,57 @@ sources_united as (
     select to_date(_created_micros) from source_interviews
     union
     select to_date(_updated_micros) from source_interviews
+),
 
+min_max as (
+    select
+        min(date) as min_date,
+        current_date() as max_date
+    from sources_united
+    where date is not null
+),
+
+date_spine as (
+    select
+        dateadd(day, seq4(), min_date) as date
+    from min_max,
+    table(generator(rowcount => 10000))  
+    where dateadd(day, seq4(), min_date) <= max_date
 ),
 
 enriched as (
-
     select
-        date,
-        year(date) as year,
-        quarter(date) as quarter,
-        month(date) as month,
-        day(date) as day,
-        week(date) as week,
-        dayofweek(date) as day_of_week,
-        dayname(date) as day_name,
-        monthname(date) as month_name,
-        not coalesce (day_of_week between 1 and 5, false) as is_weekend,
+        d.date,
+        year(d.date) as year,
+        quarter(d.date) as quarter,
+        month(d.date) as month,
+        day(d.date) as day,
+        week(d.date) as week,
+        dayofweek(d.date) as day_of_week,
+        dayname(d.date) as day_name,
+        monthname(d.date) as month_name,
+
+        not (dayofweek(d.date) between 1 and 5) as is_weekend,
+
         case
-            when month = 1 and day = 1 then TRUE --New Year’s Day
-            when month = 7 and day = 4 then TRUE --Independence Day
-            when month = 11 and day = 11 then TRUE --Veterans Day
-            when month = 12 and day = 25 then TRUE --Christmas Day
-            when month = 1 and day_of_week = 1 and day between 15 and 21 then TRUE --MLK
-            when month = 2 and day_of_week = 1 and day between 15 and 21 then TRUE --Presidents’ Day
-            when month = 5 and day_of_week = 1 and day + 7 > 31 then TRUE --Memorial Day
-            when month = 9 and day_of_week = 1 and day <= 7 then TRUE --Labor Day
-            when month = 10 and day_of_week = 1 and day between 8 and 14 then TRUE --Columbus Day
-            when month = 11 and day_of_week = 4 and day between 22 and 28 then TRUE --Thanksgiving
-            else FALSE
+            when month(d.date) = 1 and day(d.date) = 1 then true
+            when month(d.date) = 7 and day(d.date) = 4 then true
+            when month(d.date) = 11 and day(d.date) = 11 then true
+            when month(d.date) = 12 and day(d.date) = 25 then true
+            when month(d.date) = 1 and dayofweek(d.date) = 1 and day(d.date) between 15 and 21 then true
+            when month(d.date) = 2 and dayofweek(d.date) = 1 and day(d.date) between 15 and 21 then true
+            when month(d.date) = 5 and dayofweek(d.date) = 1 and day(d.date) + 7 > 31 then true
+            when month(d.date) = 9 and dayofweek(d.date) = 1 and day(d.date) <= 7 then true
+            when month(d.date) = 10 and dayofweek(d.date) = 1 and day(d.date) between 8 and 14 then true
+            when month(d.date) = 11 and dayofweek(d.date) = 4 and day(d.date) between 22 and 28 then true
+            else false
         end as is_holiday
-    from sources_united
-    where date is not null
 
-),
-
-typecasted as (
-
-    select
-        date,
-        cast(year as integer) as year,
-        cast(quarter as integer) as quarter,
-        cast(month as integer) as month,
-        cast(day as integer) as day,
-        cast(week as integer) as week,
-        cast(day_of_week as integer) as day_of_week,
-        cast(day_name as varchar(10)) as day_name,
-        cast(month_name as varchar(10)) as month_name,
-        is_weekend,
-        is_holiday
-    from enriched
-
+    from date_spine d
 ),
 
 final as (
-
-    select * from typecasted
-
+    select * from enriched
 )
 
 select * from final
